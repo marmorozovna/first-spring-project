bdc2429105e5b4d879d3c3b23ae451e27d17dafa
Марина <marina.morozevich@gmail.com>
2023-11-30 15:36:30 +0300

    enum added

--- first-spring-project/src/main/java/ru/itgirl/firstspringproject/controller/FirstController.java
+++ first-spring-project/src/main/java/ru/itgirl/firstspringproject/controller/FirstController.java
@@ -3,22 +3,17 @@ package ru.itgirl.firstspringproject.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.firstspringproject.model.DayOfWeek;
 
 @RestController
 public class FirstController {
     @GetMapping("/dayOfWeek")
-    public String writeDayOfWeek(@RequestParam(value = "name", defaultValue = "День Недели") String name) {
-        name = switch (name) {
-            case ("MONDAY") -> "понедельник";
-            case ("TUESDAY") -> "вторник";
-            case ("WEDNESDAY") -> "среда";
-            case ("THURSDAY") -> "четверг";
-            case ("FRIDAY") -> "пятница";
-            case ("SATURDAY") -> "суббота";
-            case ("SUNDAY") -> "воскресенье";
-            default -> "День Недели";
+    public String writeDayOfWeek(@RequestParam(required = false) String name) {
+        DayOfWeek day = DayOfWeek.NOT_FOUND;
+        if (name != null){
+            day = DayOfWeek.valueOf(name);
         };
-        return String.format("Сегодня %s!", name);
+        return String.format("Сегодня %s!", day);
     }
 }
 
--- first-spring-project/src/main/java/ru/itgirl/firstspringproject/model/DayOfWeek.java
+++ first-spring-project/src/main/java/ru/itgirl/firstspringproject/model/DayOfWeek.java
@@ -1,11 +1,24 @@
 package ru.itgirl.firstspringproject.model;
 
 public enum DayOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY ("понедельник"),
+    TUESDAY("вторник"),
+    WEDNESDAY("среда"),
+    THURSDAY ("четверг"),
+    FRIDAY ("пятница"),
+    SATURDAY ("суббота"),
+    SUNDAY ("воскресенье"),
+    NOT_FOUND ("День недели")
+    ;
+
+    final String name;
+
+    DayOfWeek (String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
